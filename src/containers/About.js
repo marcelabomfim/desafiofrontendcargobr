b6626cab2676f27ce7096f03a0df4eb1c6e13564
Marcela Bomfim <mbomfim@live.com>
2018-09-20 20:42:06 -0300

    About page content

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,3 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default () => <div>ABOUT PAGE</div>;
+export default () => (
+  <About>
+    <p>This is a fictional project, made for the CargoBR frontend challenge.</p>
+    <p>
+      Source code available on{' '}
+      <a
+        className="link"
+        href="https://github.com/marcelabomfim/desafiofrontendcargobr"
+      >
+        github repository
+      </a>
+    </p>
+  </About>
+);
+
+const About = styled.div`
+  text-align: center;
+
+  a {
+    color: ${({ theme }) => theme.colors.primary};
+    text-decoration: underline;
+  }
+`;
