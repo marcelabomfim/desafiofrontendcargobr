944268507c960db1cda7f7febe3c7b1d953db3e4
Marcela Bomfim <mbomfim@live.com>
2018-09-21 10:45:54 -0300

    Disable react-snapshot temporally, study better implementation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { render } from 'react-snapshot';
+import ReactDOM from 'react-dom';
 import App from 'App';
 import registerServiceWorker from 'registerServiceWorker';
 
 const rootEl = document.getElementById('root');
 
-render(<App />, rootEl);
+ReactDOM.render(<App />, rootEl);
 registerServiceWorker();
 
 if (module.hot) {
   module.hot.accept('App', () => {
     const NextApp = require('App').default;
-    render(<NextApp />, rootEl);
+    ReactDOM.render(<NextApp />, rootEl);
   });
 }
